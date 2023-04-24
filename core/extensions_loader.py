755380eda1abe9d147eb8576b73d1e2d4c24a5ab
Maxence Lagalle <contact@maxence-lagalle.fr>
2023-04-24 18:48:29 +0200

    Base Interactions.py bot

---
+++ core/extensions_loader.py
@@ -0,0 +1,23 @@
+import os
+
+from core.base import CustomClient
+
+
+def load_extensions(bot: CustomClient):
+    """Automatically load all extension in the ./extensions folder"""
+
+    bot.logger.info("Loading Extensions...")
+
+    # go through all folders in the directory and load the extensions from all files
+    # Note: files must end in .py
+    for root, dirs, files in os.walk("extensions"):
+        for _file in files:
+            if _file.endswith(".py") and not _file.startswith("__init__"):
+                _file = _file.removesuffix(".py")
+                path = os.path.join(root, _file)
+                python_import_path = path.replace("/", ".").replace("\\", ".")
+
+                # load the extension
+                bot.load_extension(python_import_path)
+
+    bot.logger.info(f"< {len(bot.interactions_by_scope.get(0, []))} > Global Interactions Loaded")
