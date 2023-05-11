9044c60721e046c77e436bbd958560a4d5d0efb8
Maxence Lagalle <contact@maxence-lagalle.fr>
2023-05-11 20:19:45 +0200

    Fix Member Update : check for roles difference length

--- extensions/member_update_event.py
+++ extensions/member_update_event.py
@@ -12,13 +12,15 @@ class MemberUpdateEvent(Extension):
     async def on_member_update(self, event: MemberUpdate):
         roles_before = set(event.before.roles) if event.before.roles else set()
         roles_after = set(event.after.roles) if event.after.roles else set()
-        role_updated = list(roles_before.symmetric_difference(roles_after))[0]
-        self.bot.logger.debug(f"Role updated : {role_updated}")
-        metaroles = await Metarole.filter(
-            guild=int(event.guild.id), enabled=True, conditions__id=role_updated
-        )
-        for metarole in metaroles:
-            await self.bot.check_metarole(metarole, [event.after])
+        roles_difference = roles_before.symmetric_difference(roles_after)
+        if len(roles_difference) > 0:
+            role_updated = list(roles_difference)[0]
+            self.bot.logger.debug(f"Role updated : {role_updated}")
+            metaroles = await Metarole.filter(
+                guild=int(event.guild.id), enabled=True, conditions__id=role_updated
+            )
+            for metarole in metaroles:
+                await self.bot.check_metarole(metarole, [event.after])
 
 
 def setup(bot: CustomClient):
